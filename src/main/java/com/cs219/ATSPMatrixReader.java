fdd30a828bec63fad42c168f52d9e3f3070ee8db
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2023-09-27 18:22:02 +1200

    added more tests

--- src/main/java/com/cs219/ATSPMatrixReader.java
+++ src/main/java/com/cs219/ATSPMatrixReader.java
@@ -10,62 +10,53 @@ import java.util.*;
 public class ATSPMatrixReader {
     public static void main(String[] args) {
         int[][] ft53 = read("src/main/resources/ft53.atsp");
-        int[][]ft70 = read("src/main/resources/ft70.atsp");
+        int[][] ft70 = read("src/main/resources/ft70.atsp");
         int[][] ftv33 = read("src/main/resources/ftv33.atsp");
-        int [][] ftv170 = read("src/main/resources/ftv170.atsp");
+        int[][] ftv170 = read("src/main/resources/ftv170.atsp");
         int[][] rbg443 = read("src/main/resources/rbg443.atsp");
-int[][] br17 = read("src/main/resources/br17.atsp");
+        int[][] br17 = read("src/main/resources/br17.atsp");
+        int[][] test5 = read("src/main/resources/p01_d.tsp");
 
-        // Display the read adjacency matrix
-        /*System.out.println("ATSP Adjacency Matrix:");
-        for (int i = 0; i < rbg443.length; i++) {
-            for (int j = 0; j < rbg443.length; j++) {
-                System.out.print(rbg443[i][j] + " ");
-            }
-            System.out.println();
-        }*/
-
-        int minTourLengthHeldKarp = heldKarp(br17);
-        int minTourLength = solveATSP(br17);
-        System.out.println("HeldKarp " + minTourLengthHeldKarp);
-        System.out.println("Vanilla " + minTourLength);
-
+        printMatrix(ftv33);
+        int minTourLengthHeldKarp = heldKarp(ftv33);
+        System.out.println("Shortest Tour " + minTourLengthHeldKarp);
     }
 
-    public static int[][] read(String filePath){
+    public static int[][] read(String filePath) {
         try {
-        Queue<Integer> entries = new LinkedList<Integer>();
+            Queue<Integer> entries = new LinkedList<Integer>();
 
 
-        // Open the file for reading
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            // Open the file for reading
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
 
-        // Read the number of cities from the first line of the file
-        int size = Integer.parseInt(reader.readLine());
-        int[][] matrix = new int[size][size];
+            // Read the number of cities from the first line of the file
+            int size = Integer.parseInt(reader.readLine());
+            int[][] matrix = new int[size][size];
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (entries.isEmpty()) {
-                    readNextLine(reader, entries);
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (entries.isEmpty()) {
+                        readNextLine(reader, entries);
+                    }
+
+                    matrix[i][j] = entries.poll();
                 }
-
-                matrix[i][j] = entries.poll();
             }
+            reader.close();
+            return matrix;
+
+            // Close the file
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
         }
-        reader.close();
-        return matrix;
-
-        // Close the file
-
-    } catch (IOException e) {
-        System.err.println("Error reading the file: " + e.getMessage());
-    }
         return null;
     }
-public static void readNextLine(BufferedReader reader, Queue<Integer> entries)
+
+    public static void readNextLine(BufferedReader reader, Queue<Integer> entries)
             throws IOException {
-        String line = null;
+        String line;
 
         do {
             line = reader.readLine();
@@ -81,16 +72,32 @@ public static void readNextLine(BufferedReader reader, Queue<Integer> entries)
             entries.offer(Integer.parseInt(tokens[i]));
         }
     }
-    public static int solveATSP(int[][] distanceMatrix) {
+
+    public static void printMatrix(int[][] matrix) {
+        // Display the read adjacency matrix
+        System.out.println("Adjacency Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int heldKarp(int[][] distanceMatrix) {
         int numCities = distanceMatrix.length;
         int numStates = 1 << numCities; // Total number of states (2^n)
 
         // Create a memoization table to store optimal tour lengths
         int[][] dp = new int[numStates][numCities];
 
-        // Initialize the memoization table with a large value (infinity)
+        // Create a table to store the parent cities for tour reconstruction
+        int[][] parent = new int[numStates][numCities];
+
+        // Initialize the memoization and parent tables
         for (int i = 0; i < numStates; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
+            Arrays.fill(parent[i], -1);
         }
 
         // Initialize the starting state with city 0
@@ -103,95 +110,58 @@ public static void readNextLine(BufferedReader reader, Queue<Integer> entries)
                     // Iterate over all possible previous cities
                     for (int v = 0; v < numCities; v++) {
                         if ((mask & (1 << v)) != 0 && v != u) {
-                            dp[mask][u] = Math.min(dp[mask][u], dp[mask ^ (1 << u)][v] + distanceMatrix[v][u]);
-                        }
-                    }
-                }
-            }
-        }
-
-        // Calculate the minimum tour length
-        int minTourLength = Integer.MAX_VALUE;
-        for (int u = 1; u < numCities; u++) {
-            minTourLength = Math.min(minTourLength, dp[numStates - 1][u] + distanceMatrix[u][0]);
-        }
-
-        return minTourLength;
-    }
-
-
-
-        public static int heldKarp(int[][] distanceMatrix) {
-            int numCities = distanceMatrix.length;
-            int numStates = 1 << numCities; // Total number of states (2^n)
-
-            // Create a memoization table to store optimal tour lengths
-            int[][] dp = new int[numStates][numCities];
-
-            // Create a table to store the parent cities for tour reconstruction
-            int[][] parent = new int[numStates][numCities];
-
-            // Initialize the memoization and parent tables
-            for (int i = 0; i < numStates; i++) {
-                Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
-                Arrays.fill(parent[i], -1);
-            }
-
-            // Initialize the starting state with city 0
-            dp[1][0] = 0;
-
-            // Iterate over all subsets of cities
-            for (int mask = 1; mask < numStates; mask += 2) {
-                for (int u = 1; u < numCities; u++) {
-                    if ((mask & (1 << u)) != 0) {
-                        // Iterate over all possible previous cities
-                        for (int v = 0; v < numCities; v++) {
-                            if ((mask & (1 << v)) != 0 && v != u) {
-                                int newDistance = dp[mask ^ (1 << u)][v] + distanceMatrix[v][u];
-                                if (newDistance < dp[mask][u]) {
-                                    dp[mask][u] = newDistance;
-                                    parent[mask][u] = v;
-                                }
+                            int newDistance = dp[mask ^ (1 << u)][v] + distanceMatrix[v][u];
+                            if (newDistance < dp[mask][u]) {
+                                dp[mask][u] = newDistance;
+                                parent[mask][u] = v;
                             }
                         }
                     }
                 }
             }
-
-            // Find the minimum tour length
-            int minTourLength = Integer.MAX_VALUE;
-            int endingCity = -1;
-            for (int u = 1; u < numCities; u++) {
-                int tourLength = dp[numStates - 1][u] + distanceMatrix[u][0];
-                if (tourLength < minTourLength) {
-                    minTourLength = tourLength;
-                    endingCity = u;
-                }
-            }
-
-            // Reconstruct the tour
-            List<Integer> tour = new ArrayList<>();
-            int mask = numStates - 1;
-            int currentCity = endingCity;
-            while (currentCity != -1) {
-                tour.add(currentCity);
-                int nextCity = parent[mask][currentCity];
-                mask ^= (1 << currentCity); // Update the mask by removing currentCity
-                currentCity = nextCity;
-            }
-            tour.add(0); // Add the starting city to complete the tour
-
-            // Reverse the tour to start from the beginning
-            List<Integer> reconstructedTour = new ArrayList<>();
-            for (int i = tour.size() - 1; i >= 0; i--) {
-                reconstructedTour.add(tour.get(i));
-            }
-
-            // Print the reconstructed tour
-            System.out.println("Reconstructed Tour: " + reconstructedTour);
-            return minTourLength;
         }
 
+        // Find the minimum tour length
+        int minTourLength = Integer.MAX_VALUE;
+        int endingCity = -1;
+        for (int u = 1; u < numCities; u++) {
+            int tourLength = dp[numStates - 1][u] + distanceMatrix[u][0];
+            if (tourLength < minTourLength) {
+                minTourLength = tourLength;
+                endingCity = u;
+            }
+        }
+
+        // Reconstruct the tour
+        List<Integer> tour = new ArrayList<>();
+        int mask = numStates - 1;
+        int currentCity = endingCity;
+        while (currentCity != -1) {
+            tour.add(currentCity);
+            int nextCity = parent[mask][currentCity];
+            mask ^= (1 << currentCity); // Update the mask by removing currentCity
+            currentCity = nextCity;
+        }
+        tour.add(0); // Add the starting city to complete the tour
+
+        // Reverse the tour to start from the beginning
+        List<Integer> reconstructedTour = new ArrayList<>();
+        for (int i = tour.size() - 1; i >= 0; i--) {
+            reconstructedTour.add(tour.get(i));
+        }
+
+        // Print the reconstructed tour
+        System.out.println("Reconstructed Tour: " + reconstructedTour);
+        return minTourLength;
+    }
+
+
 }
+
+
+
+
+
+
 
 
--- src/main/java/com/cs219/Main.java
+++
@@ -1,7 +0,0 @@
-package com.cs219;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
